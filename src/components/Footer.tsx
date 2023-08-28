6d38f3e6627d0d9577e366c7167e1b99d49c71af
Chizoba Victory <43314673+chizobavictory@users.noreply.github.com>
2023-08-28 08:31:46 +0100

    changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between dotted-pattern border-t-2 border-t-red-500 ">
       <Link href="/" className="font-bold text-xl">EAZYBITE</Link>
-      <p>© ALL RIGHTS RESERVED.</p>
+      <p className="">© CHIZOBA VICTORY.</p>
     </div>
   );
 };
