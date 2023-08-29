f746b4d412131a928b74de4aee1f7eecf0d4c18f
Chizoba Victory <43314673+chizobavictory@users.noreply.github.com>
2023-08-29 09:10:16 +0100

    added azany page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,10 +1,25 @@
-import { menu } from "@/data";
+import { MenuType } from "@/types/types";
 import Link from "next/link";
 import React from "react";
 
-const MenuPage = () => {
+const getData = async ()=>{
+  const res = await fetch("http://localhost:3000/api/categories",{
+    cache:"no-store"
+  })
+
+  if(!res.ok){
+    throw new Error("Failed!");
+    
+  }
+
+  return res.json()
+}
+
+const MenuPage = async () => {
+
+  const menu:MenuType = await getData()
   return (
-    <div className="md:p-4 p-2 lg:px-20 xl:px-40 md:h-[70vh] h-[calc(100vh-6rem)] flex flex-col md:flex-row items-center">
+    <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
       {menu.map((category) => (
         <Link
           href={`/menu/${category.slug}`}
